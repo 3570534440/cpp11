dcbb179d3fe8d23e647660f80a1ef001ec3b1140
嘉兴 <3570534440@qq.com>
2022-03-24 12:56:15 +0000

    update 3.constexpr.cpp.

--- 3.constexpr.cpp
+++ 3.constexpr.cpp
@@ -34,3 +34,68 @@ int main()
 	*/
 	
 }
+1// error，不是常量表达式函数
+constexpr void func1()
+{
+    int a = 100;
+    cout << "a: " << a << endl;
+}
+
+// error，不是常量表达式函数
+constexpr int func1()
+{
+    int a = 100;
+    return a;
+}
+// error
+constexpr int func1()
+{
+    constexpr int a = 100;
+    constexpr int b = 10;
+    for (int i = 0; i < b; ++i)
+    {
+        cout << "i: " << i << endl;
+    }
+    return a + b;
+}
+
+// ok
+constexpr int func2()
+{
+    using mytype = int;
+    constexpr mytype a = 100;
+    constexpr mytype b = 10;
+    constexpr mytype c = a * b;
+    return c - (a + b);
+}
+
+/*修饰模板函数
+
+struct Person {
+    const char* name;
+    int age;
+};
+
+// 定义函数模板
+template<typename T>
+constexpr T dispaly(T t) {
+    return t;
+}
+
+int main()
+{
+    struct Person p { "luffy", 19 };
+    //普通函数
+    struct Person ret = dispaly(p);
+    cout << "luffy's name: " << ret.name << ", age: " << ret.age << endl;
+
+    //常量表达式函数
+    constexpr int ret1 = dispaly(250);
+    cout << ret1 << endl;
+
+    constexpr struct Person p1 { "luffy", 19 };
+    constexpr struct Person p2 = dispaly(p1);
+    cout << "luffy's name: " << p2.name << ", age: " << p2.age << endl;
+    return 0;
+}
+
