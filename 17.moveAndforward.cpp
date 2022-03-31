2c37c0bcb0483506846e1db04c63a6da7496d130
仵嘉兴 <3570534440@qq.com>
2022-03-31 14:09:38 +0000

    update 17.moveAndforward.cpp.

--- 17.moveAndforward.cpp
+++ 17.moveAndforward.cpp
@@ -27,3 +27,52 @@ ls.push_back("world");
 list<string> ls1 = ls;        // 需要拷贝, 效率低
 list<string> ls2 = move(ls);
 
+
+//右值引用类型是独立于值的，一个右值引用作为函数参数的形参时，在函数内部转
+//发该参数给内部其他函数时，它就变成一个左值，并不是原来的类型了。如果需要
+//按照参数原来的类型转发到另一个函数，可以使用 C++11 提供的 std::forward () 函数
+//该函数实现的功能称之为完美转发。
+
+// 函数原型
+template <class T> T&& forward (typename remove_reference<T>::type& t) noexcept;
+template <class T> T&& forward (typename remove_reference<T>::type&& t) noexcept;
+
+// 精简之后的样子
+std::forward<T>(t);
+//fordward的使用方法
+#include <iostream>
+using namespace std;
+
+template<typename T>
+void printValue(T& t)
+{
+    cout << "l-value: " << t << endl;
+}
+
+template<typename T>
+void printValue(T&& t)
+{
+    cout << "r-value: " << t << endl;
+}
+
+template<typename T>
+void testForward(T && v)//传递参数时，右值引用会变为左值引用，但左值引用不会变为右值引用
+{
+    printValue(v);
+    printValue(move(v));
+    printValue(forward<T>(v));
+    cout << endl;
+}
+
+int main()
+{
+    testForward(520);//先右值引用 然后左值引用 然后右值引用， 然后右值引用
+    int num = 1314;
+    testForward(num);
+    testForward(forward<int>(num));//int是右值引用
+    testForward(forward<int&>(num));
+    testForward(forward<int&&>(num));
+
+    return 0;
+}
+
